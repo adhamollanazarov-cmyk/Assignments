43784c2598e1ed5f30a5691a7c5ff86415db8df6
Adhamjon Ollanazarov <adhamollanazarov.com>
2025-10-19 16:37:46 +0500

    Initial commit

---
+++ demo.py
@@ -0,0 +1,32 @@
+print("=== Pizza Shop Order System ===")
+print("Enter pizza size: personal, medium, or family")
+print("Type 'done' when finished ordering")
+total = 0.0
+while True:
+    print()
+    user_input = input("Enter pizza size: ")
+    if user_input == "personal":
+        total += 7.00
+        print("Price: $7.00")
+        print(f"Current total: ${total:.2f}")
+    elif user_input == "medium":
+        total += 12.00
+        print("Price: $12.00")
+        print(f"Current total: ${total:.2f}")
+    elif user_input == "family":
+        total += 18.00
+        print("Price: $18.00")
+        print(f"Current total: ${total:.2f}")
+    elif user_input == "done":
+        break
+    print()
+if total >= 40.00:
+    discount = 5.00
+else:
+    discount = 0.00
+final_total = total - discount
+print("=== Order Summary ===")
+print(f"Subtotal: ${total:.2f}")
+print(f"Party Order Discount: -${discount} ")
+print(f"Final Total: ${final_total:2f}")
+print("Thank you for your order!")
